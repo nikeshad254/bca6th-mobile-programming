1993f9fe91332e80a2e3c7394e23624c1f37e7df
Nikesh Adhikari <nikeshad254@gmail.com>
2024-06-07 06:26:27 +0545

    Question and Answer app UI only created

---
+++ QAApp/app/src/main/java/com/example/qaapp/Main.java
@@ -0,0 +1,14 @@
+package com.example.qaapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class Main extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.qalayout);
+    }
+}
