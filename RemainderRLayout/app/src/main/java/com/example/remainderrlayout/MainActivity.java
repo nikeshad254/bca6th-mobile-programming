6589ec26cb9e395ee67e4b045ae4cffae00d2f75
Nikesh Adhikari <nikeshad254@gmail.com>
2024-06-12 08:51:12 +0545

    remainder ui using relative layout

---
+++ RemainderRLayout/app/src/main/java/com/example/remainderrlayout/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.remainderrlayout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+
+    }
+}
