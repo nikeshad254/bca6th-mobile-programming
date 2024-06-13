3fb4b0b4531052161ab5f3d7fd0c464188965f7b
Nikesh Adhikari <nikeshad254@gmail.com>
2024-06-13 18:00:42 +0545

    remainder R layout's edit text changed to spinner and implemented using arraay adapter and create resource

--- RemainderRLayout/app/src/main/java/com/example/remainderrlayout/MainActivity.java
+++ RemainderRLayout/app/src/main/java/com/example/remainderrlayout/MainActivity.java
@@ -2,15 +2,39 @@ package com.example.remainderrlayout;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.annotation.Nullable;
 
 public class MainActivity extends Activity {
+    Spinner dateSpinner;
+    Spinner timeSpinner;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        // add item into date
+        dateSpinner = findViewById(R.id.dates);
+        ArrayAdapter<CharSequence> dateAdapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.dates_array,
+                android.R.layout.simple_spinner_item
+        );
+        dateAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dateSpinner.setAdapter(dateAdapter);
+
+
+        // add item into date
+        timeSpinner = findViewById(R.id.times);
+        ArrayAdapter<CharSequence> timeAdapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.time_array,
+                android.R.layout.simple_spinner_item
+        );
+        timeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        timeSpinner.setAdapter(timeAdapter);
 
     }
 }
