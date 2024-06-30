6ed6311bfe78ea4d42f502b709c96acb00477cb6
Nikesh Adhikari <nikeshad254@gmail.com>
2024-06-30 09:55:07 +0545

    Drawable example: shape, vector, image, state and App Icon

---
+++ DrawablesExample/app/src/main/java/com/example/drawablesexample/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.drawablesexample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
